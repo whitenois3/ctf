e885e36b007dee61cd3925ade571c590fa3fffd5
clabby <ben@clab.by>
2022-09-17 13:43:58 -0400

    Small updates

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Whitenoise CTF
 
 Contracts for the Whitenoise CTF.
+
+## TODO
+
+### General
+
+- [ ] Create reward NFT contract.
+
+### Stage 1
+
+- [ ] Create honeypot macros for dispatch table.
+
+### Stage 2
+
+- [ ] Add a macro to mint the exploiter NFT for the first solver.
+- [ ] Store leaderboard in the NFT contract, Optimizer NFTs claimable at the end of the challenge.
+- [ ] Write the optimization challenge.
+  - [ ] ...
--- test/Challenge.t.sol
+++ test/Challenge.t.sol
@@ -5,28 +5,40 @@ import {Test} from "forge-std/Test.sol";
 import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
 
 interface IChallenge {
-    // TODO
+// TODO
+}
+
+contract Solution {
+    function owner() external pure returns (address) {
+        return 0x00000000000000000000000000000000bEefbabe;
+    }
+
+    function solve(uint256) external returns (uint256) {
+        return 0; // TODO
+    }
 }
 
 contract ChallengeTest is Test {
     IChallenge public challenge;
-    
+    Solution public solution;
+
     function setUp() public {
-        challenge = IChallenge(
-            HuffDeployer.config().deploy("DovesInTheWind")
-        );
+        challenge = IChallenge(HuffDeployer.config().deploy("DovesInTheWind"));
+        solution = new Solution();
     }
 
     function testSolve() public {
         // Set tx.origin to beefbabe
         vm.startPrank(address(this), address(0x00000000000000000000000000000000bEefbabe));
 
-        // Create input with beefbabe's solution.
+        // Create input with beefbabe's magic.
         // Will need to be changed once DovesInTheWind.huff has more code above
         // the magic_dest label, as the solution input's hash contains the desired jumpdest.
         bytes32 input;
+        uint256 magic = 0x4000000010f2def3;
+        address solAddr = address(solution);
         assembly {
-            input := or(shl(0xC0, 0x4000000010f2def3), address())
+            input := or(shl(0xC0, magic), solAddr)
         }
 
         // Call the challenge's third dispatch to get access to the wildcard logic.
@@ -38,6 +50,6 @@ contract ChallengeTest is Test {
         assembly {
             returned := mload(add(a, 0x20))
         }
-        assertEq(returned, address(this));
+        assertEq(returned, address(solution));
     }
 }
